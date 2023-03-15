3dcaab31b63cf5043ca9a331f85c90af350169de
Galahallt <paolo_edni_v_espiritu@dlsu.edu.ph>
2023-03-16 01:37:03 +0800

    project updates

---
+++ CSC615M-Abstract-Machine-Interpreter/src/machine/Controller.java
@@ -0,0 +1,67 @@
+package machine;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.LineNumberFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    // -------------------- Code Area -------------------- //
+    public CodeArea caMachineDef;
+
+    // -------------------- Text Field -------------------- //
+    public TextField tfInputString;
+
+    // -------------------- Table View -------------------- //
+    public TableView<Rule> tblRules;
+    public TableView<StepLogs> tblStepLogs;
+    public TableView<Memory> tblMemory;
+
+
+    // -------------------- Buttons -------------------- //
+
+    // Machine Definition
+    public Button btnEdit;
+    public Button btnSave;
+
+    // Controls
+    public Button btnPlay;
+    public Button btnStep;
+    public Button btnReset;
+
+    // -------------------- Initialize Elements On Load -------------------- //
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        caMachineDef.setParagraphGraphicFactory(LineNumberFactory.get(caMachineDef));
+        caMachineDef.setContextMenu();
+    }
+
+    // -------------------- Action Events -------------------- //
+
+    // Machine Definition
+    public void handleEditClick() {
+        System.out.println("Edit");
+    }
+
+    public void handleSaveClick() {
+        System.out.println("Save");
+    }
+
+    // Controls
+    public void handlePlayClick() {
+        System.out.println("Play");
+    }
+
+    public void handleResetClick() {
+        System.out.println("Reset");
+    }
+
+    public void handleStepClick() {
+        System.out.println("Step");
+    }
+}
---
+++ CSC615M-Abstract-Machine-Interpreter/src/machine/Main.java
@@ -0,0 +1,33 @@
+package machine;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    Stage window;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
+        window.setTitle("Abstract Machine Interpreter");
+
+        window.setOnCloseRequest(e -> {e.consume(); closeProgram();});
+
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+
+        Scene scene = new Scene(root);
+        window.setScene(scene);
+        window.show();
+    }
+
+    public void closeProgram() {
+        window.close();
+    }
+}
